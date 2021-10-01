28f95690141d84565e6b637683d28ab4f22ef0ea
Gunashree S <gunashrees341@gmail.com>
2021-10-01 17:21:17 +0530

    Update Food.js

--- js/Food.js
+++ js/Food.js
@@ -48,16 +48,18 @@ class Food {
         }
       }
     }
+    
+    //set the background for the functions
 
     bedroom(){
-        background(bedroom,550,500);  
+        
     }
       
     garden(){
-        background(garden,550,500);  
+        
     } 
 
     washroom(){
-        background(washroom,550,500); 
+        
     }
 }
